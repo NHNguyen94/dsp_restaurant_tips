fa539fa6764959bd9bebdaf812f23271bf66d7ae
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-02 10:18:28 +0100

    add utils and unittest for utils

---
+++ src/utils/__init__.py
@@ -0,0 +1,2 @@
+from src.utils.DirectoryManager import DirectoryManager
+from src.utils.DateTimeManager import DateTimeManager
---
+++ src/utils/enum.py
@@ -0,0 +1,4 @@
+class DataPathConfigs:
+    RAW_DATA_PATH = "src/data/raw_data"
+    GOOD_DATA_PATH = "src/data/good_data"
+    BAD_DATA_PATH = "src/data/bad_data"
