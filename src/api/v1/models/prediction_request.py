283d986951b85b2aa0cc81b386d3e1c598923ab7
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-04 10:55:07 +0100

    change model schema

--- src/api/v1/models/prediction_request.py
+++ src/api/v1/models/prediction_request.py
@@ -2,11 +2,9 @@ from pydantic import BaseModel
 
 
 class PredictionRequest(BaseModel):
-    order_id: int
-    date: str
-    item_name: str
-    item_type: str
-    item_price: float
-    transaction_type: str
-    received_by: str
-    time_of_sale: str
+    total_bill: float
+    sex: str
+    smoker: bool
+    day: str
+    time: str
+    size: int
