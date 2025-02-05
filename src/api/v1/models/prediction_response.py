5920da474c9a83c80c6562e76205a20ad62c4767
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-05 14:19:17 +0100

    update response model to include features from request

--- src/api/v1/models/prediction_response.py
+++ src/api/v1/models/prediction_response.py
@@ -2,4 +2,10 @@ from pydantic import BaseModel
 
 
 class PredictionResponse(BaseModel):
+    total_bill: float
+    sex: str
+    smoker: bool
+    day: str
+    time: str
+    size: int
     tip: float
