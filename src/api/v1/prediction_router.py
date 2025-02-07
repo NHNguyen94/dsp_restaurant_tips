8810ce9921f9f8a93a9f3f5f0a77a930de0e194e
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-07 12:38:12 +0100

    refactor api input

--- src/api/v1/prediction_router.py
+++ src/api/v1/prediction_router.py
@@ -1,29 +1,34 @@
-from typing import Optional
+from typing import List, Annotated
 
-import pandas as pd
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Depends
 
-from src.api.v1.models import PredictionRequest, PredictionResponseDataFrame
+from src.api.v1.models import PredictionRequest, PredictionResponse
+from src.ml_pipelines.inference import async_predict_response_with_features
+from src.utils.api_request_parser import ApiRequestParser
 
 router = APIRouter(tags=["prediction"])
 
+api_request_parser = ApiRequestParser()
 
-@router.post("/predict", response_model=PredictionResponseDataFrame)
+
+@router.post("/predict", response_model=List[PredictionResponse])
 async def predict(
-        request: Optional[PredictionRequest] = None,
-        input_files: Optional[UploadFile] = File(None)
+        input_json: Annotated[PredictionRequest, Depends()] = None,
+        input_file: Annotated[UploadFile, File()] = None
 ):
-    df = pd.DataFrame({
-        "col1": [1, 2, 3],
-        "col2": ["A", "B", "C"]
-    })
-    return PredictionResponseDataFrame(columns=df.columns.tolist(), data=df.values.tolist())
+    if input_json != None:
+        df = api_request_parser.parse_request_to_df(input_json)
+    elif input_file != None:
+        df = await api_request_parser.parse_csv_to_df(input_file)
+    else:
+        raise ValueError("Either request body or input file must be provided.")
+
+    df_with_predictions = await async_predict_response_with_features(df)
+    print(df_with_predictions)
+    print(df_with_predictions.to_dict(orient="records"))
+    return df_with_predictions.to_dict(orient="records")
 
 
-@router.post("/past-predictions", response_model=PredictionResponseDataFrame)
-async def past_predictions(request: PredictionRequest):
-    df = pd.DataFrame({
-        "col1": [1, 2, 3],
-        "col2": ["A", "B", "C"]
-    })
-    return PredictionResponseDataFrame(columns=df.columns.tolist(), data=df.values.tolist())
+@router.post("/past-predictions", response_model=PredictionResponse)
+async def past_predictions():
+    pass
