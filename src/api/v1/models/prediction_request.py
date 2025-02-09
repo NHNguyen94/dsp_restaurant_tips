4e8602aa97f625362e9206ca2868c90afdac3e90
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-09 23:50:21 +0100

    update to have optional input for json or csv file

--- src/api/v1/models/prediction_request.py
+++ src/api/v1/models/prediction_request.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import BaseModel, model_validator
 
@@ -6,20 +6,21 @@ from fastapi import HTTPException
 
 
 class PredictionRequest(BaseModel):
-    total_bill: float
-    sex: Literal["Male", "Female"]
-    smoker: Literal["Yes", "No"]
-    day: Literal["Sun", "Sat", "Fri", "Thur", "Wed", "Tue", "Mon"]
-    time: Literal["Lunch", "Dinner"]
-    size: int
+    total_bill: Optional[float] = None
+    sex: Optional[Literal["Male", "Female"]] = None
+    smoker: Optional[Literal["Yes", "No"]] = None
+    day: Optional[Literal["Sun", "Sat", "Fri", "Thur", "Wed", "Tue", "Mon"]] = None
+    time: Optional[Literal["Lunch", "Dinner"]] = None
+    size: Optional[int] = None
 
     # Ref: https://docs.pydantic.dev/latest/api/functional_validators/#pydantic.functional_validators.model_validator
     @model_validator(mode="after")
     def validate_numbers(self):
-        if self.total_bill <= 0:
-            raise HTTPException(
-                status_code=400, detail="total_bill must be greater than 0."
-            )
-        if self.size <= 0:
-            raise HTTPException(status_code=400, detail="size must be greater than 0.")
-        return self
+        if self.total_bill is not None and self.size is not None:
+            if self.total_bill <= 0:
+                raise HTTPException(
+                    status_code=400, detail="total_bill must be greater than 0."
+                )
+            if self.size <= 0:
+                raise HTTPException(status_code=400, detail="size must be greater than 0.")
+            return self
