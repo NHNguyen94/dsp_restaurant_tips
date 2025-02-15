d2c67fc98f23eb438cf38c6b666563ceb3abae73
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-16 00:21:43 +0100

    refactor

--- src/database/session_manager.py
+++ src/database/session_manager.py
@@ -8,6 +8,7 @@ dotenv.load_dotenv()
 
 
 class SessionManager:
+    # https://sqlmodel.tiangolo.com/tutorial/fastapi/session-with-dependency/#use-the-dependency
     def __init__(self):
         self.engine = create_engine(os.getenv("DATABASE_URL"))
         self.Session = sessionmaker(bind=self.engine)
