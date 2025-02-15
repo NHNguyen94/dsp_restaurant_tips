7c5ebc55bd89ca287dc5d355626971a3abc1e6a1
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-15 20:43:37 +0100

    add db models

---
+++ src/database/models.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime
+
+from sqlmodel import SQLModel, Field
+
+from src.utils.date_time_manager import DateTimeManager
+
+
+class FileRegistration(SQLModel, table=True):
+    __tablename__ = "file_registration"
+    file_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    file_path: str = Field(max_length=255, nullable=False, unique=False)
+    created_at: datetime = Field(default_factory=DateTimeManager.get_current_utc_time, nullable=False)
+    completed_at: datetime = Field(nullable=True)
+
+
+class DataIssues(SQLModel, table=True):
+    __tablename__ = "data_issues"
+    file_id: uuid.UUID = Field(foreign_key="file_registration.file_id", primary_key=True)
+    row_missing: int = Field(nullable=False)
+    rows_unknown_value: int = Field(nullable=False)
+    rows_invalid_value: int = Field(nullable=False)
+    rows_outlier: int = Field(nullable=False)
+    features_missing: int = Field(nullable=False)
+    label_missing: int = Field(nullable=False)
+    created_at: datetime = Field(default_factory=DateTimeManager.get_current_utc_time, nullable=False)
