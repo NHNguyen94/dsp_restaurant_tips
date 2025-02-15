e11ba1cd98b3c910b94dd5e92b95035710de785a
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-15 23:06:30 +0100

    add indices for tables

--- src/database/models.py
+++ src/database/models.py
@@ -12,18 +12,18 @@ class ProjectBaseModel(SQLModel):
 
 class FileRegistration(ProjectBaseModel, table=True):
     __tablename__ = "file_registration"
-    file_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    file_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
     file_path: str = Field(max_length=255, nullable=False, unique=False)
     created_at: datetime = Field(
         default_factory=DateTimeManager.get_current_utc_time, nullable=False
     )
-    completed_at: datetime = Field(nullable=True)
+    completed_at: datetime = Field(nullable=True, index=True)
 
 
 class DataIssues(ProjectBaseModel, table=True):
     __tablename__ = "data_issues"
     file_id: uuid.UUID = Field(
-        foreign_key="file_registration.file_id", primary_key=True
+        foreign_key="file_registration.file_id", primary_key=True, index=True
     )
     row_missing: int = Field(nullable=False)
     rows_unknown_value: int = Field(nullable=False)
