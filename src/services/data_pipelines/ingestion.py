fb87fa743a58ff69800184f675c51a515c12efba
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-15 20:42:35 +0100

    add base data pipelines

--- src/services/data_pipelines/ingestion.py
+++ src/services/data_pipelines/ingestion.py
@@ -1,26 +1,17 @@
-from typing import List
-
 import pandas as pd
 from airflow import DAG
 
-from src.data_pipelines.models.bad_data import BadData, Anomaly
 from src.utils.configs_manager import DataPathConfigs
 from src.utils.directory_manager import DirectoryManager
 
 data_path_configs = DataPathConfigs.get_configs()
 
-def _load_files() -> List[str]:
-    return DirectoryManager.get_file_path_in_dir(
-        data_path_configs.RAW_DATA_PATH
-    )
 
-# Only need to read into the folder and return the file paths
 def ingest_data(dag: DAG) -> None:
     with dag:
-        files = _load_files()
-        for file in files:
-            print(f"Reading file {file}")
-            df = pd.read_csv(file)
-            print(df.head())
-
-
+        files = DirectoryManager.get_file_path_in_dir(
+            data_path_configs.RAW_DATA_PATH
+        )
+        if files is not None:
+            for file in files:
+                print(f"Reading file {file}")
---
+++ src/services/data_pipelines/validation.py
@@ -0,0 +1,8 @@
+def _compute_anomaly(self, df: pd.DataFrame) -> Anomaly:
+    return Anomaly(
+        total_null=df.isnull().sum().sum(),
+        total_missing_columns=df.isnull().any(axis=1).sum(),
+        total_wrong_dtype=0,
+        total_wrong_format=0,
+        total_not_accept_value=0,
+    )
