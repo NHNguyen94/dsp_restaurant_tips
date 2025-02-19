44f929173874cb5a401a399b0f9f031bc03ba452
NguyenNguyen <118033516+NHNguyen94@users.noreply.github.com>
2025-02-19 17:10:53 +0700

    add read and save data into ingestion pipeline (#16)

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,7 +1,10 @@
 import subprocess
+import uuid
 from typing import Dict
 
 import yaml
+
+from src.utils.date_time_manager import DateTimeManager
 
 
 def load_configs(config_path) -> Dict:
@@ -15,3 +18,11 @@ def round_number(number: float, decimal_places: int = 2) -> float:
 
 def get_current_user() -> str:
     return subprocess.check_output("whoami", encoding="utf-8").strip()
+
+
+def get_unique_id() -> str:
+    return (
+        str(uuid.uuid4())
+        + "_"
+        + DateTimeManager.get_current_local_time().replace(" ", "_")
+    )
