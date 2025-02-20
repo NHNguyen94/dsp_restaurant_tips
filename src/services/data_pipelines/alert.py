bb502accff93e3b1ef2c1ae23fde82f92654c112
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-20 10:21:55 +0100

    update alert

--- src/services/data_pipelines/alert.py
+++ src/services/data_pipelines/alert.py
@@ -8,5 +8,5 @@ def run_alert(validated_result: ValidatedResult) -> None:
     bad_df = validated_result.final_df[validated_result.final_df["is_good"] == 0]
     if bad_df.shape[0] > 0:
         ms_teams_client.send_message(
-            f"Bad data detected, details in the report: {validated_result.docs_urls}"
+            f"Bad data detected in file: {validated_result.file_path}, details in the report: {validated_result.docs_urls}"
         )
