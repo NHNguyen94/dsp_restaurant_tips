62bb44a34623a950414a11d25031af648ab80dee
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-20 10:50:39 +0100

    enhance save file task

--- src/services/data_pipelines/ingestion.py
+++ src/services/data_pipelines/ingestion.py
@@ -11,4 +11,6 @@ def run_ingest_data() -> str:
     files = DirectoryManager.get_file_path_in_dir(data_path_configs.RAW_DATA_PATH)
     if len(files) == 0:
         raise AirflowSkipException("No files to ingest")
-    return files[np.random.randint(0, len(files))]
+    file_to_ingest = files[np.random.randint(0, len(files))]
+    print(f"\nFile to ingest: {file_to_ingest}\n")
+    return file_to_ingest
--- src/services/data_pipelines/save_file.py
+++ src/services/data_pipelines/save_file.py
@@ -10,7 +10,7 @@ dir_manager = DirectoryManager()
 
 
 def _save_file(file_path: str, df: pd.DataFrame) -> None:
-    df.to_parquet(file_path, index=False)
+    df.to_csv(file_path, index=False, header=True)
 
 
 def _split_good_and_bad(df: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
@@ -21,7 +21,10 @@ def _split_good_and_bad(df: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
 
 def run_save_file(validated_result: ValidatedResult) -> None:
     file_name = validated_result.file_path.split("/")[-1]
-    file_name = file_name + "_" + get_unique_id()
-    (good_df, bad_df) = _split_good_and_bad(validated_result.final_df)
-    _save_file(data_path_configs.GOOD_DATA_PATH + "/" + file_name, good_df)
-    _save_file(data_path_configs.BAD_DATA_PATH + "/" + file_name, bad_df)
+    file_name = f"{file_name}_{get_unique_id()}.csv"
+    good_df, bad_df = _split_good_and_bad(validated_result.final_df)
+    if not good_df.empty:
+        _save_file(f"{data_path_configs.GOOD_DATA_PATH}/{file_name}", good_df)
+    if not bad_df.empty:
+        _save_file(f"{data_path_configs.BAD_DATA_PATH}/{file_name}", bad_df)
+    DirectoryManager.delete_file(validated_result.file_path)
