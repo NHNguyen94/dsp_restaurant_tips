4225532e3f5984618fc5574b07bcb048b334ea06
DeltaDave12 <david.rossig12@gmail.com>
2025-02-21 15:07:23 +0100

    Modifications for demo : modified Makefile split-dataset command, generated files elsewhere for test and remove files after test

--- src/services/data_pipelines/split_data.py
+++ src/services/data_pipelines/split_data.py
@@ -2,13 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import sys
-# Get the absolute path of the project root directory
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
-# Add the project root to the Python path
-sys.path.append(project_root)
-# Import the function
 from src.utils.helper import get_unique_id
-
 
 def split_data(filePath, folderPath, nbrOfFiles):
     # Load data
@@ -21,5 +15,6 @@ def split_data(filePath, folderPath, nbrOfFiles):
     for i in range(nbrOfFiles):
         data_split[i].to_csv(os.path.join(folderPath, 'data_' + get_unique_id() + '.csv'), index=False)
 
-
-split_data('src/data/raw_data/dataset.csv', 'src/data/generated_data', 25)
+#Only do that if file is executed, not when its imported
+if __name__ == "__main__":
+    split_data('src/data/raw_data/dataset.csv', 'src/data/generated_data', int(sys.argv[1]))
