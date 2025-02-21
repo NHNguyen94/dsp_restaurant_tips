80d83e37afb6c63a28a6954769a5d7e960484eec
David Rossig <91453740+DeltaDave12@users.noreply.github.com>
2025-02-21 15:09:14 +0100

    split dataset functionnality
    * v1 of spliting dataset
    
    * adding hidden file to acknowledge directory existence
    
    * Modifications for demo : modified Makefile split-dataset command, generated files elsewhere for test and remove files after test

---
+++ tests/test_split_data.py
@@ -0,0 +1,17 @@
+from src.services.data_pipelines.split_data import split_data
+from src.utils.directory_manager import DirectoryManager
+import os
+
+class TestSplitData:
+    def test_split_data(self):
+        rawDataPath = 'src/data/raw_data/dataset.csv'
+        folderPath = 'tests/resources/generated_data'
+        numberOfFiles = 25
+        curr_file_count = len([f for f in os.listdir(folderPath) if os.path.isfile(os.path.join(folderPath, f))])
+        split_data(rawDataPath, folderPath, numberOfFiles)
+        real_file_count = len([f for f in os.listdir(folderPath) if os.path.isfile(os.path.join(folderPath, f))]) - curr_file_count
+        assert real_file_count == numberOfFiles
+        generated_files = DirectoryManager.get_file_path_in_dir('tests/resources/generated_data')
+        for i in range(len(generated_files)):
+            if '.gitkeep' not in generated_files[i]:
+                DirectoryManager.delete_file(generated_files[i])
