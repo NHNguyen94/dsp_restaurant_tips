3cd7c7b11688fd837b1d0b846b5b8f6e3eb190d9
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-23 00:56:19 +0100

    refactor

--- src/services/data_pipelines/__init__.py
+++ src/services/data_pipelines/__init__.py
@@ -1,5 +1,5 @@
-from src.services.data_pipelines.ingestion import run_ingest_data
+from services.data_pipelines.ingest.ingestion import run_ingest_data
 
-from src.services.data_pipelines.validation import run_validate_data
-from src.services.data_pipelines.save_file import run_save_file
-from src.services.data_pipelines.alert import run_alert
+from services.data_pipelines.ingest.validation import run_validate_data
+from services.data_pipelines.ingest.save_file import run_save_file
+from services.data_pipelines.ingest.alert import run_alert
