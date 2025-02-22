b5f9978435c0043113c88ea026d99f04dd749910
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-23 00:56:12 +0100

    refactor db structure

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime
 
 from sqlmodel import SQLModel, Field
@@ -11,30 +10,13 @@ class ProjectBaseModel(SQLModel):
     pass
 
 
-class FileRegistration(ProjectBaseModel, table=True):
-    __tablename__ = "file_registration"
-    file_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    file_path: str = Field(max_length=255, nullable=False, unique=False)
+class PredictedFiles(ProjectBaseModel, table=True):
+    __tablename__ = "predicted_files"
+    file_name: str = Field(primary_key=True, index=True)
     created_at: datetime = Field(
         default_factory=DateTimeManager.get_current_utc_time, nullable=False
     )
     completed_at: datetime = Field(nullable=True, index=True)
-
-
-class DataIssues(ProjectBaseModel, table=True):
-    __tablename__ = "data_issues"
-    file_id: uuid.UUID = Field(
-        foreign_key="file_registration.file_id", primary_key=True, index=True
-    )
-    row_missing: int = Field(nullable=False)
-    rows_unknown_value: int = Field(nullable=False)
-    rows_invalid_value: int = Field(nullable=False)
-    rows_outlier: int = Field(nullable=False)
-    features_missing: int = Field(nullable=False)
-    label_missing: int = Field(nullable=False)
-    created_at: datetime = Field(
-        default_factory=DateTimeManager.get_current_utc_time, nullable=False
-    )
 
 
 def get_engine():
