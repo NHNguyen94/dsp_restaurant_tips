4bc47a3835703eef03418bb455032879deab1073
nguyen <nguyenhainguyen2606@gmail.com>
2025-02-26 13:23:46 +0100

    update test validation

--- tests/test_validation_service.py
+++ tests/test_validation_service.py
@@ -1,4 +1,4 @@
-from src.services.data_pipelines.models.validated_result import ValidatedResult
+from src.services.data_pipelines.models import ValidatedResult
 from src.services.data_pipelines.ingest.validation import ValidationService
 
 
@@ -10,7 +10,8 @@ class TestValidationService:
             self.failed_test_csv_path, "failed batch"
         )
         result = validation_service.validate_data()
+        # print(f"\nresult_test_validate_data: {result}")
         df = result.final_df
         total_good_cols = df["is_good"].sum()
-        # assert isinstance(result, ValidatedResult)
-        # assert total_good_cols == 1
+        assert isinstance(result, ValidatedResult)
+        assert total_good_cols == 1
