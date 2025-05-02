9b3ccce2753acf3b053576b2683594912cd811c2
nguyen <nguyenhainguyen2606@gmail.com>
2025-05-02 13:48:35 +0200

    update table schema

--- src/services/data_pipelines/ingest/report.py
+++ src/services/data_pipelines/ingest/report.py
@@ -15,7 +15,7 @@ def _parse_validated_results_with_good_csv(
     missing_values = 0
     duplicated_rows = 0
     unknown_categorical_values = 0
-    unknon_numeric_values = 0
+    unknown_numeric_values = 0
     total_rows = len(validated_result.final_df)
     total_bad_rows = len(
         validated_result.final_df[
@@ -37,21 +37,32 @@ def _parse_validated_results_with_good_csv(
                         all_res.result_between_or_in_set.unexpected_count
                     )
                 if res.column in ["total_bill", "size"]:
-                    unknon_numeric_values += (
+                    unknown_numeric_values += (
                         all_res.result_between_or_in_set.unexpected_count
                     )
+
+    # if there is a missing column => The whole dataset is invalid
+    if missing_columns > 0:
+        success_percent = 0
+        unsuccessful_expectations = validated_result.overall_statistics.evaluated_expectations
+        successful_expectations = 0
+    else:
+        success_percent = validated_result.overall_statistics.success_percent
+        unsuccessful_expectations = validated_result.overall_statistics.unsuccessful_expectations
+        successful_expectations = validated_result.overall_statistics.successful_expectations
+
 
     return DataIssues(
         file_path=validated_result.file_path,
         evaluated_expectations=validated_result.overall_statistics.evaluated_expectations,
-        successful_expectations=validated_result.overall_statistics.successful_expectations,
-        unsuccessful_expectations=validated_result.overall_statistics.unsuccessful_expectations,
-        success_percent=validated_result.overall_statistics.success_percent,
+        successful_expectations=successful_expectations,
+        unsuccessful_expectations=unsuccessful_expectations,
+        success_percent=success_percent,
         missing_columns=missing_columns,
         missing_values=missing_values,
         duplicated_rows=duplicated_rows,
         unknown_categorical_values=unknown_categorical_values,
-        unknon_numeric_values=unknon_numeric_values,
+        unknon_numeric_values=unknown_numeric_values,
         bad_csv_encoding=0,
         bad_csv_format=0,
         other_parse_issues=0,
